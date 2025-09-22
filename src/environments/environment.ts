7d98042eb4e26ffc604b0534815596a28cbb7736
naukarina <naukarina@gmail.com>
2025-09-22 13:13:23 +0200

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'kreola-tops-new',
+    appId: '1:121945248569:web:e078cb33abde50e3ffcee8',
+    storageBucket: 'kreola-tops-new.appspot.com', // Corrected URL
+    apiKey: 'AIzaSyCCGM7UwGg9D7NGRm31-72QFaBRvT7yPTI',
+    authDomain: 'kreola-tops-new.firebaseapp.com',
+    messagingSenderId: '121945248569',
+    measurementId: 'G-XK0FJ0ML81',
+  },
+};

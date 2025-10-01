193b05de74c9f95fcd71c9ca754ccfee54c0d9b3
naukarina <naukarina@gmail.com>
2025-10-01 14:48:28 +0200

    list design

--- src/app/pages/products/product-list/product-list.ts
+++ src/app/pages/products/product-list/product-list.ts
@@ -9,11 +9,21 @@ import { ProductService } from '../../../services/product.service';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox'; // Import MatCheckboxModule
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatTableModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule, // Add MatCheckboxModule here
+    MatCardModule,
+  ],
   templateUrl: './product-list.html',
   styleUrls: ['./product-list.scss'],
 })
@@ -21,7 +31,8 @@ export class ProductListComponent implements OnInit {
   private productService = inject(ProductService);
 
   products$!: Observable<Product[]>;
-  displayedColumns: string[] = ['productCode', 'name', 'actions'];
+  // Add 'select' to the beginning of the array
+  displayedColumns: string[] = ['select', 'productCode', 'name', 'actions'];
 
   ngOnInit(): void {
     this.products$ = this.productService.getProducts();
